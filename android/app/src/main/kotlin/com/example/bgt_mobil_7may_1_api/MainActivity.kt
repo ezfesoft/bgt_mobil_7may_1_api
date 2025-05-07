09567e40650650323abfde3e28da8ed4746f08e2
bgt <bgt@giresun.edu.tr>
2025-05-07 08:50:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bgt_mobil_7may_1_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bgt_mobil_7may_1_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: avoid_print, sized_box_for_whitespace, prefer_interpolation_to_compose_strings
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+
+  @override
+  Widget build(BuildContext context) {
+    
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------j
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("API Baðlantisi"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+            //-----------------
+          ],
+        ),
+      ),);
+  }
+}
